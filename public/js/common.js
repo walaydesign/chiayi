f7c6feab04cca0968c1c31f8a6dc9d64c67a070d
youzhen <yl301114@gmail.com>
2025-06-07 22:14:03 +0100

    首頁

--- public/js/common.js
+++ public/js/common.js
@@ -8,3 +8,12 @@ $(".header_nav_item.has-child>a").click(function() {
         $(this).parents(".header_nav_item").find(".header_nav_second").slideToggle(300);
     }
 })
+
+$(".header_menu").click(function() {
+    $(this).toggleClass("active");
+    $(".header_nav").slideToggle(300);
+})
+
+$(".header_top").click(function(){
+    $("html, body").animate({scrollTop: 0},300);
+})
